d2e040b575881c15ac3778d2c32a8c19979473af
Simon Castagna <simon@kosli.com>
2026-01-12 11:07:05 +0100

    Calculate price with tax

--- src/app/routes.py
+++ src/app/routes.py
@@ -7,11 +7,15 @@ def register_routes(app: Flask) -> None:
         if request.method == "POST":
             item_price = float(request.form["item_price"])
             number_of_items = int(request.form["number_of_items"])
-            total_price = item_price * number_of_items
+            total_price = round(item_price * number_of_items, 2)
+            ut_tax_rate = 6.85
+            total_price_with_tax = round(total_price * (1 + ut_tax_rate / 100), 2)
             return render_template(
                 "index.html",
                 item_price=item_price,
                 number_of_items=number_of_items,
                 total_price=total_price,
+                total_price_with_tax=total_price_with_tax,
+                ut_tax_rate=ut_tax_rate,
             )
         return render_template("index.html")
