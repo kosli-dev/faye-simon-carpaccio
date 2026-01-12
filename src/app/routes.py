57580dda0d41a153de718268960428bfa4daa7c4
Simon Castagna <simon@kosli.com>
2026-01-12 11:03:19 +0100

    First few slices

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 def register_routes(app: Flask) -> None:
-    @app.route("/")
+    @app.route("/", methods=["GET", "POST"])
     def index():
+        if request.method == "POST":
+            item_price = float(request.form["item_price"])
+            number_of_items = int(request.form["number_of_items"])
+            total_price = item_price * number_of_items
+            return render_template(
+                "index.html",
+                item_price=item_price,
+                number_of_items=number_of_items,
+                total_price=total_price,
+            )
         return render_template("index.html")
