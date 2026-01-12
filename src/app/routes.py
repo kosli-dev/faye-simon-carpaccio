3d15472883d2102c353bfe996870bb8d0f8eff19
Faye <faye@kosli.com>
2026-01-12 11:44:12 +0100

    Apply correct discount for different total price

--- src/app/routes.py
+++ src/app/routes.py
@@ -5,18 +5,26 @@ def register_routes(app: Flask) -> None:
     @app.route("/", methods=["GET", "POST"])
     def index():
         states = {"UT": 6.85, "NV": 8.00, "TX": 6.25, "AL": 4.00, "CA": 8.25}
+        discounts = {50000: 0.15, 10000: 0.10, 7000: 0.07, 5000: 0.05, 1000: 0.03}
         state_list = [state for state in states.keys()]
-        print(state_list)
+
+        actual_discount_rate = 0
+        discount_amount = 0
+
         if request.method == "POST":
             item_price = float(request.form["item_price"])
             number_of_items = int(request.form["number_of_items"])
             total_price = round(item_price * number_of_items, 2)
             tax_rate = states[request.form["state"]]
-            total_price_with_tax = round(total_price * (1 + tax_rate / 100), 2)
 
-            if total_price > 1000:
-                discount = 0.03
-                total_price = round(total_price * (1 - discount), 2)
+            for discount_threshold, discount_rate in discounts.items():
+                if total_price >= discount_threshold:
+                    actual_discount_rate = discount_rate * 100
+                    discount_amount = round(total_price * discount_rate, 2)
+                    total_price = round(total_price * (1 - discount_rate), 2)
+                    break
+                
+            total_price_with_tax = round(total_price * (1 + tax_rate / 100), 2)
 
             return render_template(
                 "index.html",
@@ -27,5 +35,7 @@ def register_routes(app: Flask) -> None:
                 total_price=total_price,
                 total_price_with_tax=total_price_with_tax,
                 tax_rate=tax_rate,
+                discount_rate=actual_discount_rate,
+                discount_amount=discount_amount,
             )
         return render_template("index.html", states=state_list)
